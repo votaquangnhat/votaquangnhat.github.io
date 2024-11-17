e5987d7484edce28a732f834777348ed6752c1b0
Vo Ta Quang Nhat <votaquangnhat1104@gmail.com>
2024-11-17 15:04:09 +0700

    Quartz sync: Nov 17, 2024, 3:04 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "votaquangnhat.github.io",
     ignorePatterns: ["media", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
