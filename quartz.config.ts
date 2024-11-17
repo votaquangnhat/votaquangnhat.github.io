9d23aac82ef9c4627041845f916e2ee63aad4ec4
Vo Ta Quang Nhat <votaquangnhat1104@gmail.com>
2024-11-17 14:59:54 +0700

    Quartz sync: Nov 17, 2024, 2:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 vtqn's garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["media", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
     theme: {
