8cdfd4f21725aafc49d0b94a32ff83778d9012d0
Vo Ta Quang Nhat <votaquangnhat1104@gmail.com>
2025-03-11 02:20:01 +0700

    Quartz sync: Mar 11, 2025, 2:20 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,9 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
+      GitHub: "https://github.com/votaquangnhat/",
+      Facebook: "https://facebook.com/votaquangnhat",
+    },  
   }),
 }
 
